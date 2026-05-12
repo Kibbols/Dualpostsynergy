3eccca7cd0b6969e5433d17835bb3b615aae26db
Kibbols <sponge9031@gmail.com>
2026-05-11 22:32:28 -0500

    Update TIKTOK_CLIENT_KEY in config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   GOOGLE_CLIENT_ID: '684633849089-qasr9779deg5mg2a00tb28pt1q9g1nus.apps.googleusercontent.com',
-  TIKTOK_CLIENT_KEY: 'YOUR_TIKTOK_CLIENT_KEY',
+  TIKTOK_CLIENT_KEY: 'awkup588am1r9phe',
   WORKER_URL: 'https://withered-mud-dc44.portgamingsttv.workers.dev',
   REDIRECT_URI: 'https://kibbols.github.io/Dualpostsynergy',
 };
